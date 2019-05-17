d07812109d562f868fc8c23db1baeb1d361e2f33
Anthony Williams <anthony.williams.cs@gmail.com>
2019-05-16 23:01:40 -0600

    Add JSX version of TodoList

---
+++ mobx-fun/src/TodoList.jsx
@@ -0,0 +1,40 @@
+// import {
+// 	// action,
+// 	autorun,
+// 	computed,
+// 	observable
+// } from 'mobx'
+import React from 'react'
+import { observer } from 'mobx-react'
+
+// import { TodoStore } from './TodoStore'
+
+// interface ITodoListProps {
+// 	store: TodoStore
+// }
+
+const TodoView = observer(props => null)
+
+const TodoList = observer(props => {
+	const { store } = props
+	const onNewTodo = () => {
+		store.addTodo(prompt('Enter a new todo:', 'coffee plz'))
+	}
+
+	return (
+		<div>
+			{store.report}
+			<ul>
+				{store.todos.map((todo, index) => <TodoView todo={todo} key={index} />)}
+			</ul>
+			{store.pendingRequests > 0 ? <marquee>Loading...</marquee> : null}
+			<button onClick={onNewTodo}>New Todo</button>
+			<small> (double-click a todo to edit)</small>
+			{/*
+			<RenderCounter />
+			*/}
+		</div>
+	)
+})
+
+export { TodoList }
