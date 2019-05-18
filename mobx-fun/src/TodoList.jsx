f8654b8414e3f41af3d84b88d66c52d640cce9bd
Anthony Williams <anthony.williams.cs@gmail.com>
2019-05-17 23:35:50 -0600

    Abandon observer pattern (use observer decorator)

--- mobx-fun/src/TodoList.jsx
+++ mobx-fun/src/TodoList.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { observer } from 'mobx-react'
 
 const TodoView = observer(props => null)
 
-const TodoList = observer(props => {
-	const { store } = props
-	const onNewTodo = () => {
-		store.addTodo(prompt('Enter a new todo:', 'coffee plz'))
+@observer
+class TodoList extends Component {
+	onNewTodo = () => {
+		this.props.store.addTodo(prompt('Enter a new todo:', 'coffee plz'))
 	}
 
-	return (
-		<div>
-			{store.report}
-			<ul>
-				{store.todos.map((todo, index) => <TodoView todo={todo} key={index} />)}
-			</ul>
-			{store.pendingRequests > 0 ? <marquee>Loading...</marquee> : null}
-			<button onClick={onNewTodo}>New Todo</button>
-			<small> (double-click a todo to edit)</small>
-			{/*
-			<RenderCounter />
-			*/}
-		</div>
-	)
-})
+	render() {
+		const { store } = this.props
+		return (
+			<div>
+				{store.report}
+				<ul>
+					{store.todos.map((todo, index) => <TodoView todo={todo} key={index} />)}
+				</ul>
+				{store.pendingRequests > 0
+					? <div>Loading...</div>
+					: null
+				}
+				<button onClick={this.onNewTodo}>New Todo</button>
+				<small> (double-click a todo to edit)</small>
+				{/*
+					<RenderCounter />
+				*/}
+			</div>
+		)
+	}
+}
 
 export { TodoList }
