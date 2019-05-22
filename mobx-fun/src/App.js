5b604aa827780240b977e9ef5f3c5736b6c68cf6
Anthony Williams <anthony.williams.cs@gmail.com>
2019-05-21 22:36:22 -0600

    Cleanup App

--- mobx-fun/src/App.js
+++ mobx-fun/src/App.js
@@ -1,40 +1,28 @@
 import React, { Component } from 'react'
 
-//components
 import Emoji from './Emoji'
 import { TodoList } from './TodoList'
 import { TodoStore } from './TodoStore'
 
 import logo from './logo.svg'
 
-//styles
 import './App.scss'
-import './App.less'
-import './App.styl'
-
-//modules
-import cssStyles from './First.module.css'
-// import sassStyles from './Second.module.scss'
-// import lessStyles from './Third.module.less'
-// import stylusStyles from './Fourth.module.styl'
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<div className={cssStyles.header}>
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2 className="App-title">
-						<Emoji label="danger" emoji="☢" />
-						<span> custom-react-scripts </span>
+			<article className="app">
+				<section className={cssStyles.header}>
+					<img src={logo} className="logo" alt="logo" />
+					<h2 className="title">
 						<Emoji label="danger" emoji="☢" />
 					</h2>
-					<div className="App-subtitle">
+					<div className="subtitle">
 						allow custom config for create-react-app without ejecting
 					</div>
-				</div>
+				</section>
 				<TodoList store={new TodoStore()} />
-			</div>
+			</article>
 		)
 	}
 }
