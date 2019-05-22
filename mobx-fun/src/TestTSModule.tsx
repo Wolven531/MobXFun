635a3347908c5ed542a49642721d06243f0c0095
Anthony Williams <anthony.williams.cs@gmail.com>
2019-05-21 22:46:12 -0600

    Add single TS module to play with

---
+++ mobx-fun/src/TestTSModule.tsx
@@ -0,0 +1,11 @@
+import React, { Component, ReactNode } from 'react'
+
+class TestTSModule extends Component {
+	public render(): ReactNode {
+		return (
+			<div>Tester</div>
+		)
+	}
+}
+
+export { TestTSModule }
